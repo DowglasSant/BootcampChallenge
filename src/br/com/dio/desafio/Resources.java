8a0374704a709b70a604a5594d842d746913fba7
DowglasSant <dowglas.santana9@gmail.com>
2022-02-14 23:34:26 -0300

    Second commit

---
+++ src/br/com/dio/desafio/Resources.java
@@ -0,0 +1,13 @@
+package br.com.dio.desafio;
+
+public class Resources {
+
+    public void menu(){
+        System.out.println("=========== Bem Vindo ===========\n\n");
+        System.out.println("1- Adicionar curso\n");
+        System.out.println("2- Listar cursos\n");
+        System.out.println("3- Adicionar mentoria\n");
+        System.out.println("4- Listar mentorias\n");
+        System.out.println("5- Sair\n");
+    }
+}
--- src/main.java
+++ src/main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.Contents;
-import br.com.dio.desafio.Courses;
-import br.com.dio.desafio.Mentorship;
+import br.com.dio.desafio.*;
 
 import javax.swing.*;
 import java.util.ArrayList;
@@ -14,31 +12,66 @@ public class main {
         Mentorship mentorship = new Mentorship();
         List<Contents> cursos = new ArrayList<>();
         List<Contents> mentorias = new ArrayList<>();
+        Resources recursos = new Resources();
         int continuar = 1;
         int escolha;
 
         while (continuar == 1) {
 
+            recursos.menu();
             escolha = Integer.parseInt(JOptionPane.showInputDialog("Digita a opção desejada:\n"));
 
-            switch(escolha){
+            switch (escolha) {
                 case 1:
                     cursos.add(course.novoCurso());
+                    break;
                 case 2:
-                    for (Contents conteudo : cursos){
-                        System.out.println(conteudo);}
+                    for (Contents conteudo : cursos) {
+                        System.out.println(conteudo);
+                    }
+                    break;
                 case 3:
                     mentorias.add(mentorship.novaMentoria());
+                    break;
                 case 4:
-                    for(Contents conteudo : mentorias){
-                    System.out.println(conteudo);}
+                    for (Contents conteudo : mentorias) {
+                        System.out.println(conteudo);
+                    }
+                    break;
                 case 5:
-
+                    System.out.println("Programa encerrado.\n");
+                    continuar = 2;
+                    break;
+                default:
+                    System.err.println("Opção inválida.\n");
+                    break;
             }
+        }
 
-                continuar = Integer.parseInt(JOptionPane.showInputDialog("Adicionar novo curso?\n1- Sim\n2- Nao\n"));}
+        Bootcamp bootcamp1 = new Bootcamp();
+        bootcamp1.setNome("Bootcamp Java Developer");
+        bootcamp1.setDescricao("Bootcamp de 100h sobre Java");
+        bootcamp1.getContents().addAll(cursos);
+        bootcamp1.getContents().addAll(mentorias);
 
-        System.out.println(cursos.toString());
+        Dev devLuis = new Dev();
+        devLuis.setNome("Luis");
+
+        Dev devCarlos = new Dev();
+        devCarlos.setNome("Carlos");
+
+        System.out.println("Conteudos inscritos Luis:\n" + devLuis.getCursosIncritos());
+        devLuis.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteudos inscritos Luis:\n" + devLuis.getCursosIncritos());
+        System.out.println("Conteudos concluidos Luis:\n" + devLuis.getCursoConcluidos());
+
+        System.out.println("Conteudos inscritos Carlos:\n" + devCarlos.getCursosIncritos());
+        devCarlos.inscreverBootcamp(bootcamp1);
+        System.out.println("Conteudos inscritos Luis:\n" + devCarlos.getCursosIncritos());
+        System.out.println("Conteudos concluidos Luis:\n" + devCarlos.getCursoConcluidos());
+        devCarlos.progredir();
+        System.out.println("Conteudos concluidos Luis:\n" + devCarlos.getCursoConcluidos());
+
 
         }
     }
